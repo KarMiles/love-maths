2dda16090ac7ef45d35017a04418ac4a6cc451c8
Karol Mileszko <karol.miles@gmail.com>
2021-12-30 09:37:24 +0000

    Add empty functions to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame {
+
+}
+
+function checkAnswer {
+
+}
+
+function calculateCorrectAnswer {
+
+}
+
+function calculateScore {
+
+}
+
+function incrementWrongAnswer {
+    
+}
+
+function displayAdditionQuestion {
+
+}
+
+function displaySubtractQuestion {
+
+}
+
+function displayMultiplyQuestion {
+    
+}
